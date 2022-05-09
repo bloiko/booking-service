6a26bb6439d8a0f3a9e8166ed0806334b140e5b0
Loiko <bohdan.loiko@gmail.com>
2022-05-09 16:04:41 +0300

    adding new integration tests with messaging sync

--- src/test/java/com/example/order/OrderControllerIT.java
+++ src/test/java/com/example/order/OrderControllerIT.java
@@ -17,6 +17,7 @@ import com.example.order.pojos.Ticket;
 import com.example.order.repository.OrderRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONException;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
@@ -24,10 +25,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.SimpleMessageConverter;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
@@ -36,80 +40,73 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.jms.ConnectionFactory;
+
 
 @RunWith(SpringRunner.class)
+@PropertySource("classpath:application.properties")
+
 @ContextConfiguration(
         classes = { DbConfiguration.class, JmsConfig.class},
         loader = AnnotationConfigContextLoader.class)
 @Transactional
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.MOCK, classes={ TicketApplication.class })
+@SpringBootTest(classes = TicketApplication.class)
 public class OrderControllerIT {
 
-    @Value("9000")
-    private int port;
 
     @Autowired
     OrderRepository orderRepository;
 
-    TestRestTemplate restTemplate = new TestRestTemplate();
-
-    HttpHeaders headers = new HttpHeaders();
+    @Autowired
+    JmsTemplate jmsTemplate;
 
     @Autowired
-    private WebApplicationContext webApplicationContext;
+    OrderController orderController;
 
-    private MockMvc mockMvc;
-    @BeforeEach
-    public void setup() throws Exception {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
-    }
+
 
     @Test
     public void testRetrieveOrders() throws Exception {
         Date date = new Date(2022, 20, 20);
         Order order = buildOrder(date, 1L, "source", "destination", "testUsername");
 
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-
-        this.mockMvc.perform(
-                post("/orders", "John Doe").contentType(APPLICATION_JSON_UTF8)
-                        .content(new ObjectMapper().writeValueAsString(order)));
+        orderController.orderBook(order);
 
         List<Order> all = orderRepository.findAll();
         Order order2 = all.get(0);
         Ticket ticket2 = order2.getTicket();
-        assertEquals("testUsername", order2.getUsername());
-        assertEquals("source", ticket2.getSource());
-        assertEquals("destination", ticket2.getDestination());
-        assertEquals(date, ticket2.getDate());
-        assertEquals(order.getTicket().getPrice(), ticket2.getPrice());
+        Assert.assertEquals("testUsername", order2.getUsername());
+        Assert.assertEquals("source", ticket2.getSource());
+        Assert.assertEquals("destination", ticket2.getDestination());
+        Assert.assertEquals(date, ticket2.getDate());
+        Assert.assertEquals(order.getTicket().getPrice(), ticket2.getPrice());
 
     }
 
-    @Test
-    public void addCourse() {
-        Date date = new Date(2022, 20, 20);
-        Order order = buildOrder(date, 1L, "source", "destination", "testUsername");
-
-        HttpEntity<Order> entity = new HttpEntity<Order>(order, headers);
-
-        ResponseEntity<String> response = restTemplate.exchange(
-                createURLWithPort("/order"),
-                HttpMethod.POST, entity, String.class);
-
-        List<Order> all = orderRepository.findAll();
-        Order order2 = all.get(0);
-        Ticket ticket2 = order2.getTicket();
-        assertEquals("testUsername", order2.getUsername());
-        assertEquals("source", ticket2.getSource());
-        assertEquals("destination", ticket2.getDestination());
-        assertEquals(date, ticket2.getDate());
-        assertEquals(order.getTicket().getPrice(), ticket2.getPrice());
-
-    }
-
-    private String createURLWithPort(String uri) {
-        return "http://localhost:" + port + uri;
-    }
+//    @Test
+//    public void addCourse() {
+//        Date date = new Date(2022, 20, 20);
+//        Order order = buildOrder(date, 1L, "source", "destination", "testUsername");
+//
+//        HttpEntity<Order> entity = new HttpEntity<Order>(order, headers);
+//
+//        ResponseEntity<String> response = restTemplate.exchange(
+//                createURLWithPort("/order"),
+//                HttpMethod.POST, entity, String.class);
+//
+//        List<Order> all = orderRepository.findAll();
+//        Order order2 = all.get(0);
+//        Ticket ticket2 = order2.getTicket();
+//        assertEquals("testUsername", order2.getUsername());
+//        assertEquals("source", ticket2.getSource());
+//        assertEquals("destination", ticket2.getDestination());
+//        assertEquals(date, ticket2.getDate());
+//        assertEquals(order.getTicket().getPrice(), ticket2.getPrice());
+//
+//    }
+//
+//    private String createURLWithPort(String uri) {
+//        return "http://localhost:" + port + uri;
+//    }
 
 }
